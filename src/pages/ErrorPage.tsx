21772a6c473f8d49e8a8846636f65bb6a9eef3a6
webNastya <49766412+webNastya@users.noreply.github.com>
2024-04-28 23:40:20 +0300

    Added redirect from error page to main page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
 export const ErrorPage = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        setTimeout(() => navigate("/"), 2000)
+    }, [])
     return (
-        <div></div>
+        <div>Что-то пошло не так 😿</div>
     )
 }
